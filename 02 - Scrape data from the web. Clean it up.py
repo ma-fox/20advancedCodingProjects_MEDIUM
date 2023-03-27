bb4f64830342aa145a4a5001c81f359a39489fc7
Marco Fox <marco.a.fox@gmail.com>
2023-03-27 13:41:54 +0200

    Step 2: Clean up the data

--- 02 - Scrape data from the web. Clean it up.py
+++ 02 - Scrape data from the web. Clean it up.py
@@ -6,3 +6,10 @@ url = "https://en.wikipedia.org/wiki/Python_(programming_language)"
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 text = soup.get_text()
+
+# Step 2: Clean up the data
+lines = text.split("\n")
+cleaned_text = ""
+for line in lines:
+    if len(line.strip()) > 0:
+        cleaned_text += line.strip() + " "
