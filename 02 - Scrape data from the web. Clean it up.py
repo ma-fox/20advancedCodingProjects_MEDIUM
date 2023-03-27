c46fe80f1569d4c350226d26fa1b5ebc6bbf91b9
Marco Fox <marco.a.fox@gmail.com>
2023-03-27 13:45:44 +0200

    Step 3: added commentary on wordcloud library

--- 02 - Scrape data from the web. Clean it up.py
+++ 02 - Scrape data from the web. Clean it up.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from wordcloud import WordCloud
 
 # Step 1: Scrape data from the web
 url = "https://en.wikipedia.org/wiki/Python_(programming_language)"
@@ -13,3 +14,11 @@ cleaned_text = ""
 for line in lines:
     if len(line.strip()) > 0:
         cleaned_text += line.strip() + " "
+
+# Step 3: Load data into a data analysis tool (in this case, a word cloud)
+# once the WordCloud object is created, it generates the word cloud image based
+# on the frequency of words in the input text. The image can then be saved or
+# displayed using matplotlib or other visualization libraries.
+
+wordcloud = WordCloud(width=800, height=800,
+                      background_color='white').generate(cleaned_text)
