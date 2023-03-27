4f6069838d459640025ab3f3e31c5574193dc9f5
Marco Fox <marco.a.fox@gmail.com>
2023-03-27 13:50:59 +0200

    Step 4: Build visualizations, displaying

--- 02 - Scrape data from the web. Clean it up.py
+++ 02 - Scrape data from the web. Clean it up.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 # Step 1: Scrape data from the web
 url = "https://en.wikipedia.org/wiki/Python_(programming_language)"
@@ -20,9 +21,15 @@ for line in lines:
 # on the frequency of words in the input text. The image can then be saved or
 # displayed using matplotlib or other visualization libraries.
 
+# dont forget to install wordcloud library via pip install wordcloud
 wordcloud = WordCloud(width=800, height=800,
                       background_color='white').generate(cleaned_text)
 
 # Step 4: Build visualizations
 # The matplotlib library is used to display the word cloud visualization,
 # which shows the frequency of words in the cleaned text as a graphical representation.
+plt.figure(figsize=(8, 8), facecolor=None)
+plt.imshow(wordcloud)
+plt.axis("off")
+plt.tight_layout(pad=0)
+plt.show()
